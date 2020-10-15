d000a95d48659815c1866d990393a6d61267f3ef
alfodz <alf@neo.net.ua>
2020-10-15 17:52:23 +0300

    force commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello4444444");
     }
 }
