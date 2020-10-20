22a27a0f2112f4c2dd995c6b9ecd6657f548f26a
alfodz <alf@neo.net.ua>
2020-10-20 15:56:27 +0300

    Revert "aaaaaaa"
    This reverts commit 4e71ca385826e4a6695815c7f944f0a1d7e91ae4.

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,5 @@ public class Main {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
-        System.out.println("hello3");
     }
 }
